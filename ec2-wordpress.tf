5ebdb721641d1fa13439a7668ed25cad9dd67be9
EC2 Default User <ec2-user@ip-172-31-34-217.eu-west-1.compute.internal>
2023-06-15 00:08:24 +0000

    some changes

--- ec2-wordpress.tf
+++ ec2-wordpress.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "web" {
   availability_zone      = "us-east-1a"
   ami                    = "ami-04a0ae173da5807d3"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.terraform-ec2.id]
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
   user_data              = base64encode(file("install_apache.sh"))
 
   subnet_id                   = aws_subnet.public1.id
@@ -22,34 +22,34 @@ resource "aws_instance" "web" {
 }
 
 # 3. Create sec group
-resource "aws_security_group" "terraform-ec2" {
-  name        = "terraform-ec2"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.Project.id
+# resource "aws_security_group" "terraform-ec2" {
+#   name        = "terraform-ec2"
+#   description = "Allow TLS inbound traffic"
+#   vpc_id      = aws_vpc.Project.id
 
-  ingress {
-    description = "SSH access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "SSH access"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "HTTP"
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+# }
 
