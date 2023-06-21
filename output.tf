61eeabf6108bd3c9619f14ba647933650b7e774e
EC2 Default User <ec2-user@ip-172-31-34-217.eu-west-1.compute.internal>
2023-06-21 18:45:29 +0000

    added readme

--- output.tf
+++ output.tf
@@ -2,9 +2,6 @@ output "database_endpoint" {
   value = aws_rds_cluster.db_instance.endpoint
 }
 
-output "load_balancer_arn" {
-  value = aws_alb.application-lb.arn
-}
 
 output "aws_alb_dns_name" {
   value = aws_alb.application-lb.dns_name
@@ -14,6 +11,6 @@ output "database_writer_endpoint" {
   value = aws_rds_cluster_instance.writer_instance[0].endpoint
 }
 
-output "instance_id" {
-value = aws_instance.wordpress.id
+output "instance_public_ip" {
+value = aws_instance.wordpress.public_ip
 }
